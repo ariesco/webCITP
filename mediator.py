5a8c11d5272213c05ed74629443cfd2db110d17a
ariesco <ariesco@fdi.ucm.es>
2017-07-26 17:52:21 +0200

    More tests

--- mediator.py
+++ mediator.py
@@ -5,77 +5,117 @@ Spyder Editor
 This is a temporary script file.
 """
 
-import asyncio
-
-import websockets
-
-#==============================================================================
-# import socket
-# import sys
-#==============================================================================
-
 from subprocess import Popen, PIPE, STDOUT
 
+from websocket_server import WebsocketServer
+
+#==============================================================================   
+# res = proc.stdin.write(b"red 3 .\n")
+# proc.stdin.flush()
+# outs = proc.stdout
+# print(outs.readline())
+# print(outs.readline())
+# print(outs.readline())
+# print(outs.readline())
+# print(outs.readline())
+# print(outs.readline())
+# print(outs.readline())
+# print(outs.readline())
+# print(outs.readline())
+# print(outs.readline().decode("utf-8"))
+# 
+# res = proc.stdin.write(b"red 4 .\n")
+# proc.stdin.flush()
+# res = proc.stdin.write(b"red 5 .\n")
+# proc.stdin.flush()
+# outs = proc.stdout
+# print(outs.readline())
+# print(outs.readline())
+# print(outs.readline())
+# print(outs.readline())
+# print(outs.readline())
+# print(outs.readline())
+# print(outs.readline())
+# print(outs.readline())
 #==============================================================================
-# async def citp_mediator(websocket, path):
-#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     server_address = ('127.0.0.1', 8811)
-#     sock.connect(server_address)
-#     sock.sendall(b"10")
-#     data = sock.recv(16)
-#     print(data)
+
+#==============================================================================
+# async def citp_mediator(ws, path):
+#     path = '/Applications/maude-darwin/maude.darwin64'
+#     proc = Popen(path, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+#     clean_header(proc)
+#     
+#     while True:
+#     
+#         msg = await ws.recv()
+#         msg_bytes = bytes(msg, "utf-8")
+#     
+# #==============================================================================
+# #         proc.stdin.write(msg_bytes)
+# #         proc.stdin.flush()
+# #     
+# #         outs = proc.stdout
+# #         print(outs.readline())
+# #==============================================================================
+#     
+#         print("Received '%s'" % msg)
+#     
+#     
+# 
 #==============================================================================
 #    while True:
 #        now = datetime.datetime.utcnow().isoformat() + 'Z'
-#        await websocket.send(now)
+#        await ws.send(now)
 #        await asyncio.sleep(100)
 
-path = '/Applications/maude-darwin/maude.darwin64'
-proc = Popen(path, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
-    
-res = proc.stdin.write(b"red 3 .\n")
-proc.stdin.flush()
-outs = proc.stdout
-print(outs.readline())
-print(outs.readline())
-print(outs.readline())
-print(outs.readline())
-print(outs.readline())
-print(outs.readline())
-print(outs.readline())
-print(outs.readline())
-print(outs.readline())
-print(outs.readline().decode("utf-8"))
 
-res = proc.stdin.write(b"red 4 .\n")
-proc.stdin.flush()
-res = proc.stdin.write(b"red 5 .\n")
-proc.stdin.flush()
-outs = proc.stdout
-print(outs.readline())
-print(outs.readline())
-print(outs.readline())
-print(outs.readline())
-print(outs.readline())
-print(outs.readline())
-print(outs.readline())
-print(outs.readline())
+def clean_header(maude):
+    outs = maude.stdout
+    for i in range(9):
+        print(outs.readline())
+
+# Called for every client connecting (after handshake)
+def new_client(client, server):
+    add = client['address'][0]
+    if not (add in maude_dict):
+        path = '/Applications/maude-darwin/maude.darwin64'
+        maude = Popen(path, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+        maude.stdin.write(b"load /Users/adrian/Documents/Maude/webCITP/citp.maude\n")
+        maude.stdin.flush()
+        clean_header(maude)
+        maude_dict[add] = maude
+        print("Nuevo")
+
+# Called for every client disconnecting
+def client_left(client, server):
+    print("Cerrado")
+
+# Called when a client sends a message
+def message_received(client, server, msg):
+    msg.replace("\r\n", "\n")
+    msg_bytes = bytes(msg, "utf-8")
+    add = client['address'][0]
+    maude = maude_dict[add]
+    print(msg)
+#==============================================================================
+    maude.stdin.write(msg_bytes)
+    maude.stdin.flush()
+    maude.stdin.write(b"\n")
+    maude.stdin.flush()
+    maude.stdin.write(b"red 3 .\n")
+    maude.stdin.flush()
+    outs = maude.stdout
+    print(outs.readline())
+    print(outs.readline())
+#==============================================================================
 
 
-async def citp_mediator(websocket, path):
-    path = '/Applications/maude-darwin/maude.darwin64'
-    proc = Popen(path, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
-    
-    
-    outs, errs = proc.communicate(input=b"red 3 .\n", timeout=15)
-    print(outs)
-    print(errs)
-#    while True:
-#        now = datetime.datetime.utcnow().isoformat() + 'Z'
-#        await websocket.send(now)
-#        await asyncio.sleep(100)
+maude_dict = {}
 
-# start_server = websockets.serve(citp_mediator, '127.0.0.1', 5678)
 
-# asyncio.get_event_loop().run_until_complete(start_server)
-# asyncio.get_event_loop().run_forever()
+PORT = 5678
+server = WebsocketServer(PORT)
+server.set_fn_new_client(new_client)
+server.set_fn_client_left(client_left)
+server.set_fn_message_received(message_received)
+server.run_forever()
